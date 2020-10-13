b70d692a579fd612dea92100d2301737099e00fd
LeahESc <leahemily1@gmail.com>
2020-10-13 10:46:31 -0700

    Done.

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
        end 
     end 
 
-    get '/say/:word1/:word2/:word3/:word4/:word5'
+    get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
