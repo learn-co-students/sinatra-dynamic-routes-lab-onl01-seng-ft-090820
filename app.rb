8def4a40ed6a664a7556628e86f0962bc93dbbbe
doosemavis <moosedavis2011@gmail.com>
2020-10-15 13:32:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -9,21 +9,26 @@ class App < Sinatra::Base
 
 
   get '/square/:number' do 
-
+    @square = params[:number].to_i * params[:number].to_i
+    "#{@square}"
   end 
 
 
   get '/say/:number/:phrase' do
-
+    str = ""
+    @num = params[:number].to_i
+    @phr = params[:phrase]
+    @num.times { str += "#{@phr}\n" }
+    str
   end 
 
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end 
 
 
   get '/:operation/:number1/:number2' do 
-
+    
   end 
 end
